fddd485e8f493e6c01ff6c3bb6b456e0e7595bf4
willpz09 <willpz09@yahoo.com>
2018-09-04 10:31:08 -0400

    Update main.js

--- email/js/main.js
+++ email/js/main.js
@@ -1,4 +1,13 @@
-function abc()
+//Adjust copyright date yearly
+function Copyright(startYear)
 {
-	
+	var endYear = new Date().getFullYear();
+	if(startYear == endYear)
+	{
+		document.write(startYear);
+	}
+	else
+	{
+		document.write(startYear + " - " + endYear);
+	}
 }
