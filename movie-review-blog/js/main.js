9fa0d33907a7a6462e20bc6ab4a781f30b34fb3a
willpz09 <willpz09@yahoo.com>
2018-09-12 13:43:09 -0400

    Update main.js

--- movie-review-blog/js/main.js
+++ movie-review-blog/js/main.js
@@ -1,4 +1,29 @@
 function myFunction(x)
 {
-    x.classList.toggle("change");
+	x.classList.toggle("change");
+}
+
+function ShowHide()
+{
+	this.classList.toggle("active");
+	var panel = this.nextElementSibling;
+	if (panel.style.maxHeight)
+	{
+		panel.style.maxHeight = null;
+	}
+	else
+	{
+		panel.style.maxHeight = panel.scrollHeight + "px";
+	} 
+}
+
+window.onload = function()
+{
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+	
+	for (i = 0; i < acc.length; i++)
+	{
+		acc[i].addEventListener("click", ShowHide);
+	}
 }
