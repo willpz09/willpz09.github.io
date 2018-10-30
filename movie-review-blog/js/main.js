0f85c84e1a4969551a31918636c930883772e53e
willpz09 <willpz09@yahoo.com>
2018-10-30 14:30:46 -0400

    Update main.js

--- movie-review-blog/js/main.js
+++ movie-review-blog/js/main.js
@@ -1,6 +1,29 @@
-function myFunction(x)
+function ToggleNav(x)
 {
 	x.classList.toggle("change");
+	
+	if (document.getElementById("mySidenav").style.width == "250px")
+	{
+		CloseNav();
+	}
+	else
+	{
+		OpenNav();
+	}
+}
+
+function OpenNav()
+{
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function CloseNav()
+{
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
 }
 
 function ShowHide()
